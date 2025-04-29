a52b8379b18a92e56d423197ecb7ef0dab62fc55
aMRAH MaLiKZADa <MaLiKZADa.aMRAH.ANAR.2024@asoiu.edu.az>
2025-04-29 15:50:52 +0400

    Add project files.

---
+++ kurs2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime vaxt = DateTime.Now;
+
+           
+            
+
+            Console.WriteLine(vaxt.ToString(""));
+            Console.WriteLine(vaxt.ToLongDateString());
+            Console.WriteLine(vaxt.ToLongTimeString());
+            Console.WriteLine(vaxt.AddDays(10));
+
+            TimeSpan ts = new TimeSpan();
+            
+
+
+            Console.WriteLine("Dogum tarixim: ");
+            DateTime dogumTarixi = new DateTime(2006, 10, 14);
+            Console.WriteLine(dogumTarixi.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Heftenin gunu: ");
+            Console.WriteLine(dogumTarixi.DayOfWeek);
+            Console.WriteLine("Nece gundu yasiyiram: ");
+            ts = vaxt - dogumTarixi;
+            Console.WriteLine(Math.Floor(ts.TotalDays));
+
+
+        }
+    }
+}
